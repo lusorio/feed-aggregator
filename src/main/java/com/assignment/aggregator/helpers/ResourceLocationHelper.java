3c22070e719c86df767fa2742e86b7c4f519fec3
luso <lmarqueze@gmail.com>
2021-01-21 16:33:34 +0100

    better resource location builder

--- src/main/java/com/assignment/aggregator/helpers/ResourceLocationHelper.java
+++ src/main/java/com/assignment/aggregator/helpers/ResourceLocationHelper.java
@@ -1,9 +1,11 @@
 package com.assignment.aggregator.helpers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Map;
+import java.text.MessageFormat;
 
 /**
  * This helper provides a way to build the API path being called by the current
@@ -12,23 +14,37 @@ import java.util.Map;
 public class ResourceLocationHelper
 {
 
+    private static final Logger logger = LoggerFactory.getLogger(ResourceLocationHelper.class);
+
     private ResourceLocationHelper()
     {
         // hide public constructor
     }
 
     /**
-     * Builds the complete API path for the current {@link org.springframework.web.context.request.WebRequest}
+     * Builds the API path for the current {@link org.springframework.web.context.request.WebRequest}
+     * <p>
+     * For every request, this helper can return the complete path of the endpoint being called. It is useful
+     * to determine the location of newly created resources, which should be included in the Location header
+     * of the response in POST request, where the response is 201 Created.
      *
-     * @param uriVariables a map containing the path variable names along their values
+     * @param variable
+     * @param value
      * @return the complete API path for the current {@link org.springframework.web.context.request.WebRequest}
      */
-    public static URI getResourceLocation(Map<String, Object> uriVariables)
+    public static URI getResourceLocation(String variable, Object value)
     {
-        return ServletUriComponentsBuilder
-                       .fromCurrentRequest()
-                       .pathSegment(uriVariables.keySet().toArray(String[]::new))
-                       .buildAndExpand(uriVariables)
-                       .toUri();
+        var location = ServletUriComponentsBuilder
+                               .fromCurrentRequest()
+                               .pathSegment(variable)
+                               .buildAndExpand(value)
+                               .toUri();
+
+        if (logger.isInfoEnabled())
+        {
+            logger.info(MessageFormat.format("Current location : {0}", location));
+        }
+
+        return location;
     }
 }
