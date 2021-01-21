a3247b125c5f6d143b19500589678ca56f35df88
luso <lmarqueze@gmail.com>
2021-01-21 16:36:41 +0100

    Perform channel's update directly from repository

--- src/main/java/com/assignment/aggregator/repositories/IChannelRepository.java
+++ src/main/java/com/assignment/aggregator/repositories/IChannelRepository.java
@@ -2,8 +2,11 @@ package com.assignment.aggregator.repositories;
 
 import com.assignment.aggregator.models.Channel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.ZonedDateTime;
 import java.util.Optional;
 
 @Repository
@@ -17,4 +20,14 @@ public interface IChannelRepository extends JpaRepository<Channel, Long>
      * @return an {@link Optional} of the channel
      */
     Optional<Channel> findOneByUrl(String url);
+
+    /**
+     * Updates the last refresh date for a channel
+     *
+     * @param channelId the id of the channel to update
+     * @param when      the {@link ZonedDateTime} value to set
+     */
+    @Modifying
+    @Query("update Channel ch set ch.lastRefresh = ?2 where ch.id = ?1")
+    void updateRefreshTime(long channelId, ZonedDateTime when);
 }
